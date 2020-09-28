29f9755f2fa2a2c8a160682568a5b72db8cf896a
miriamprister <66062986+miriamprister@users.noreply.github.com>
2020-09-28 14:55:45 +0200

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#this notebook will be mainly used for the capstone project
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+#Print the following the statement: Hello Capstone Project Course!
+print("Hello Capstone Project Course!")
+
